31d5b64c07cbdd7e88352598d1739d44fb5e6ac5
giumoralejo <140681527+giumoralejo@users.noreply.github.com>
2023-08-27 21:57:23 -0500

    Update T3.java

--- src/JavaGroupProject1/T3.java
+++ src/JavaGroupProject1/T3.java
@@ -11,7 +11,7 @@ public class T3 {
         for (int i = 0; i < num.length; i++) {
             for (int j = 0; j < num[i].length; j++) {
 
-                if (num[i][j]%2 !=0){
+                if (num[i][j]%2 ==0){
                     System.out.print(num[i][j]+" ");
                 }
             }
